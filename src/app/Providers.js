b7100ce8f3fc956d43cbf0be3ecdebcd83877e74
Aleksandar Atanasov <aleks930819@gmail.com>
2023-05-09 11:09:06 +0300

    added about page

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -5,7 +5,11 @@ import { ThemeProvider } from 'next-themes';
 const Providers = ({ children }) => {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 text-gray-700 transition-all duration-300 ease-in-out select-none">
+      <div
+        className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 text-gray-700 transition-all duration-300 ease-in-out select-none
+        min-h-screen
+      "
+      >
         {children}
       </div>
     </ThemeProvider>
---
+++ src/app/about/page.js
@@ -0,0 +1,52 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 space-y-6">
+      <h1 className="text-2xl text-center text-amber-600 font-bold mt-10">
+        About
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste odit amet
+        libero odio eum provident ratione unde officia repellendus commodi, ea
+        atque ex sapiente facilis voluptatibus illo minus dolor eaque. Vel
+        tenetur quos repudiandae porro asperiores nam vitae ea et, placeat quas
+        quasi quis voluptatum accusantium debitis nisi est doloremque ipsam
+        rerum nemo, ducimus voluptates. Quas at adipisci ad iure? Neque nulla
+        aliquid earum est corrupti quia voluptatem libero recusandae dolor autem
+        consequatur laborum, quibusdam, assumenda sed exercitationem rem aperiam
+        accusamus repellat pariatur sunt vitae voluptate magni nesciunt?
+        Repellendus, nihil. Quae provident repudiandae libero quia, consequatur
+        non? At ipsum illum, pariatur et neque natus quia, magnam minus
+        doloremque quae, eveniet odio. Amet, ad numquam. Aliquid quisquam
+        reprehenderit explicabo quo mollitia! Vel beatae repellendus labore,
+        nobis inventore quam omnis nisi nulla cupiditate magnam sit magni,
+        officiis soluta deleniti explicabo alias rerum. Vitae atque accusamus
+        culpa nesciunt explicabo veniam, impedit neque aperiam! Ut quibusdam
+        sint sit doloremque quam consequatur provident saepe inventore magnam
+        culpa molestias voluptas, deleniti sed repudiandae iure necessitatibus
+        labore autem nostrum enim non magni nemo at debitis nam. Rem?
+        Praesentium commodi vitae, deleniti inventore modi delectus, hic ratione
+        harum, dolor explicabo nostrum distinctio! Quos illum eum laudantium
+        doloremque dolore voluptas quisquam delectus rerum suscipit fugiat
+        repellat, eveniet ea quidem. Sit repellendus libero, nostrum itaque
+        nobis quas placeat maxime magni officia quam possimus, excepturi
+        quibusdam enim doloribus, sequi consequatur. At, quam! Blanditiis at
+        aperiam voluptas magni vitae natus exercitationem hic. Architecto quos
+        reprehenderit ut minima perspiciatis consequatur asperiores dolore iste
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis
+        ratione quas illum officia asperiores? Soluta vel doloribus aliquid,
+        iusto aliquam voluptas ducimus fugit ab accusamus? Maxime, totam.
+        Tempora, voluptate esse!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        mollitia eos enim vel blanditiis quisquam voluptatum, voluptatibus
+        debitis ipsam tenetur accusamus temporibus labore aperiam quibusdam
+        veniam veritatis dolore reiciendis sed?
+      </p>
+    </div>
+  );
+};
+
+export default About;
