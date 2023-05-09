416562ad16526d8861a2803426d6cd4df468defc
Aleksandar Atanasov <aleks930819@gmail.com>
2023-05-09 11:03:10 +0300

    added dark and light theme

---
+++ src/app/Providers.js
@@ -0,0 +1,15 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+
+const Providers = ({ children }) => {
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 text-gray-700 transition-all duration-300 ease-in-out select-none">
+        {children}
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default Providers;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
-import Header from './componets/Header';
+import Header from './components/Header';
+import Providers from './Providers';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -13,13 +14,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Header */}
-        <Header />
+        <Providers>
+          {/* Header */}
+          <Header />
 
-        {/* Navbar */}
+          {/* Navbar */}
 
-        {/* SearchBox */}
-        {children}
+          {/* SearchBox */}
+          {children}
+        </Providers>
       </body>
     </html>
   );
